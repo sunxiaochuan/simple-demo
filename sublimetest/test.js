fcc86d4483353ef502ccdcc241f0a2977abf5121
xiaochuan sun <xiaochuan sun>
2017-12-15 00:18:11 +0800

    修改 sublimetest -> test.js 文件

--- sublimetest/test.js
+++ sublimetest/test.js
@@ -1,4 +1,4 @@
 // 用多个选择进行批编辑。 在这里，按Ctrl + Shift + L是用来分割一个选择进线，每线然后同时编辑。
 days =  ["Mon","Thu","Mon","Thu","Mon","Thu","Mon","Thu"];
 //操作步骤：
-// Ctrl + Shift + L   选择多行，Shift + " ，键盘移动至最后一位
+// 选择多行，Ctrl + Shift + L  ， Shift + " ，键盘上右键向右移动至最后一位 添加 ,	，键盘上右键再向右移动一位  再按 backspace 退格键 ，然后 ctrl+ L  、左键移动 输入变量名什么的就可以了
